d700a23f36e436d78c016b26ceee417a8514fdd6
Peter Callioni <pcallioni@hotmail.com>
2025-12-22 21:10:38 +1000

    Move the score and history panels.

---
+++ OpenFun_Core/Converters/BooleanAndConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace OpenFun_Core.Converters
+{
+    public class BooleanAndConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool result = values.Select(value =>
+            {
+                if (value is bool boolValue)
+                {
+                    return boolValue;
+                }
+                return false;
+            })
+            .All(x => x == true);
+
+            return result;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OpenFun_Core/Converters/BooleanOrConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace OpenFun_Core.Converters
+{
+    public class BooleanOrConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (var value in values)
+            {
+                if (value is bool boolValue && boolValue)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
